029be048230bc83e82fdff62fe585803ac0b299b
Jay Bienvenu <bienvenunet@yahoo.com>
2016-02-17 17:49:45 -0600

    Refactor navigation. Add routes.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 export class App {
   configureRouter(config, router) {
-    config.title = 'Aurelia';
+    config.title = 'Song Charts';
     config.map([
-      { route: ['','welcome'], name: 'welcome', moduleId: './welcome', nav: true, title:'Welcome' }
+      { route: ['','welcome'], name: 'welcome', moduleId: './welcome', nav: true, title:'Welcome' },
+      { route: 'artists',      name: 'artists', moduleId: './artists', nav: true, title:'Artists' },
+      { route: 'genres',       name: 'genres', moduleId: './genres', nav: true, title:'Genres' },
+      { route: 'locations',    name: 'locations', moduleId: './locations', nav: true, title:'Locations' },
+      { route: 'songs',        name: 'songs', moduleId: './songs', nav: true, title:'Songs' },
+      { route: 'sources',      name: 'sources', moduleId: './sources', nav: true, title:'Sources' }
     ]);
 
     this.router = router;
