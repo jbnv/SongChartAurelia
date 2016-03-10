a9f5e3662c3927acbcf78c0bd53225fbf7fa8728
Jay Bienvenu <bienvenunet@yahoo.com>
2016-03-10 16:00:24 -0600

    Fix source view.

--- src/sources.js
+++ src/sources.js
@@ -1,1 +1,13 @@
-export class Sources {}
+import {Data} from './data';
+
+export class Sources extends Data {
+  fetchRoute = 'sources';
+  heading = 'Sources';
+
+  sources = [];
+
+  massage = (inbound) => {
+    this.sources = inbound;
+    console.log("sources:massage",inbound);
+  }
+}
