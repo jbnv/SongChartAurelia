e096bfdbbca5bcc4013de0bd273f13b4a7ab5afe
Jay Bienvenu <bienvenunet@yahoo.com>
2016-03-18 10:35:55 -0500

    Welcome view: Reformat and add counts.

--- src/welcome.js
+++ src/welcome.js
@@ -1,1 +1,12 @@
-export class Welcome {}
+import {Data} from './data';
+
+export class Welcome extends Data {
+  fetchRoute = 'summary';
+  title = 'Summary';
+
+  summary = {};
+
+  massage = (inbound) => {
+    this.summary = inbound;
+  }
+}
