5ceee0fa7386b038f9282fb73db3a4bd975ad750
Jay Bienvenu <bienvenunet@yahoo.com>
2016-03-18 21:41:49 -0500

    Add sort.

--- src/genres.js
+++ src/genres.js
@@ -5,6 +5,10 @@ export class Genres extends Data {
   title = 'Genres';
 
   genres = {};
+  content = function(value) {
+    if (value) this.genres = value;
+    return this.genres;
+  }
 
   keys() {
     return Object.keys(this.genres);
@@ -13,4 +17,12 @@ export class Genres extends Data {
   massage = (inbound) => {
     this.genres = inbound;
   }
+
+  sortColumn = "title";
+  sortOrder = true; // true = normal; false = reverse;
+
+  sortByTitle()       { this.sort("title"); }
+  sortBySongCount()   { this.sort("songCount"); }
+  sortByArtistCount() { this.sort("artistCount"); }
+  sortByScore()       { this.sort("score"); }
 }
