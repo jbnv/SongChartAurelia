62107f8b36757cbbef746a286687cdccd0bd0c65
Jay Bienvenu <bienvenunet@yahoo.com>
2016-03-24 08:47:22 -0500

    Data and formatting changes.

--- src/artist.js
+++ src/artist.js
@@ -6,22 +6,26 @@ export class Artist extends Data {
 
   title = '(Artist)';
   complete = false;
-  type = "";
+  type = {slug: "", title: "NOT SET"};
   genres = [];
   birth = "";
   death = "";
   origin = "";
   songs = [];
+  score = 0.00;
+  songAdjustedAverage = 0.00;
 
   massage = (inbound) => {
     this.title = inbound.title;
     this.complete = inbound.complete;
-    this.type = inbound.type;
+    this.type = inbound.type || {slug: "", title: "NOT SET"};
     this.genres = inbound.genres || [];
     this.birth = inbound.birth;
     this.death = inbound.death;
     this.origin = inbound.origin;
     this.songs = inbound.songs || [];
+    this.score = inbound.score || 0.00;
+    this.songAdjustedAverage = inbound.songAdjustedAverage || 0.00;
   }
 
 }
