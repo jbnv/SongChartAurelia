55d7312d626cca3a66e527fddb844b6c3d38438f
Jay Bienvenu <bienvenunet@yahoo.com>
2016-04-01 15:17:00 -0500

    Activate search nav.

--- src/nav-bar.js
+++ src/nav-bar.js
@@ -1,15 +1,46 @@
 import {inject} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
 
-if (!String.prototype.startsWith) {
-    String.prototype.startsWith = function(searchString, position){
-      position = position || 0;
-      return this.substr(position, searchString.length) === searchString;
-  };
-}
-
 @inject(Router)
 export class NavBar {
+
+  searchTerm = "";
+
+  filterRoutes = [
+    "artists","genres","locations",
+    "playlists","songs","sources"
+  ];
+
+  // get currentFragment() {
+  //   let history = this.router.history;
+  //   let fragment = history.fragment;
+  //   console.log(this.router.history.fragment);
+  //   return fragment;
+  // }
+
+  get currentFragmentIsFilter() {
+    // let currentFragment = this.currentFragment;
+    // let filterCheck = this.filterRoutes.filter(
+    //   route => currentFragment.startsWith("/"+route)
+    // );
+    // return filterCheck.length == 0;
+    return false;
+  }
+
+  searchButtonClass() {
+    return this.currentFragmentIsFilter ? "fa fa-filter" : "fa fa-search";
+  }
+
+  search() {
+    if (this.currentFragmentIsFilter) {
+      let currentRouteName = "TODO";
+      let currentRoute = this.router.routes.find(x => x.name === currentRouteName);
+      currentRoute.name = username;
+      this.router.navigateToRoute('userprofile');
+    } else {
+      this.router.navigateToRoute('search',{slug:this.searchTerm});
+    }
+  }
 
   constructor(router) {
     this.router = router;
