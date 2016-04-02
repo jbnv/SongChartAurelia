731ee0e7f406913ec5403991d489c81c25f93036
Jay Bienvenu <bienvenunet@yahoo.com>
2016-04-02 18:37:56 -0500

    Add fraction bars.

--- src/artist-types.js
+++ src/artist-types.js
@@ -3,4 +3,20 @@ import {Collection} from './collection';
 export class ArtistTypes extends Collection {
   fetchRoute = 'artist-types';
   title = 'Artist Types';
+  maxArtistCount = 0;
+  maxArtistAdjustedAverage = 0;
+
+  massage = (inbound) => {
+    this.content = inbound;
+    this.sortByTitle();
+    console.log(inbound);
+    inbound.forEach(item => {
+      if (item.artistCount > this.maxArtistCount) {
+        this.maxArtistCount = item.artistCount;
+      }
+      if (item.artistAdjustedAverage > this.maxArtistAdjustedAverage) {
+        this.maxArtistAdjustedAverage = item.artistAdjustedAverage;
+      }
+    });
+  }
 }
