6c317d6889cf566e9c9a03c8fc4021aaf9eaaed4
Jay Bienvenu <bienvenunet@yahoo.com>
2016-04-04 20:50:30 -0500

    Add graphs.

--- src/decades.js
+++ src/decades.js
@@ -5,8 +5,6 @@ export class Decades extends Collection {
 
   fetchRouteFn = (parameters) => 'eras'; // returns array of songs
 
-  artists = [];
-
   columns = new Columns({
     'title': 'Name',
     'songCount': 'Songs',
@@ -14,16 +12,27 @@ export class Decades extends Collection {
     'songAdjustedAverage': 'SAA',
   });
 
+  countScales = [];
+  aaScales = [];
+
   massage = (inbound) => {
     this.content = [];
-    Object.keys(inbound.decades).forEach(key => {
+    this.countScales = [];
+    this.aaScales = [];
+
+    Object.keys(inbound.decades).sort().forEach(key => {
+
       let decade = inbound.decades[key];
       decade.slug = key;
       decade.title = key;
       this.content.push(decade);
+
+      this.countScales.push({title: decade.title, scale: decade.songCountScale});
+      this.aaScales.push({title: decade.title, scale: decade.songAdjustedAverageScale});
+
     });
+
     this.aggregate();
-    this.content = this.content.sort((a,b) => a.slug > b.slug);
   }
 
 }
--- src/years.js
+++ src/years.js
@@ -12,14 +12,27 @@ export class Years extends Collection {
     'songAdjustedAverage': 'SAA',
   });
 
+  countScales = [];
+  aaScales = [];
+
   massage = (inbound) => {
     this.content = [];
+    this.countScales = [];
+    this.aaScales = [];
+
     Object.keys(inbound.years).sort().forEach(key => {
+
       let year = inbound.years[key];
       year.slug = key;
       year.title = key;
       this.content.push(year);
+
+      var title = ""+year.title.substr(2,1)+" "+year.title.substr(3,1);
+      this.countScales.push({title: title, scale: year.songCountScale});
+      this.aaScales.push({title: title, scale: year.songAdjustedAverageScale});
+
     });
+
     this.aggregate();
   }
 
