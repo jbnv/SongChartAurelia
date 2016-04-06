840ed8f460c078315a76134932d0ae0993429331
Jay Bienvenu <bienvenunet@yahoo.com>
2016-04-06 10:02:43 -0500

    Build out song view.

--- src/song.js
+++ src/song.js
@@ -4,4 +4,35 @@ export class Song extends Data {
 
   fetchRouteFn = (parameters) => 'song/'+parameters.slug;
 
+  massage = (inbound) => {
+
+    // Pull out ranks and add them to their respective entities.
+    // (Could this maybe go on the server side?)
+    Object.keys(this.ranks).forEach(key => {
+      let rank = this.ranks[key];
+      let keyParts = key.split(":");
+      let typeSlug = keyParts[0];
+      let instanceSlug = keyParts[1];
+      let entity = null;
+      switch (typeSlug) {
+        case "artist":
+          entity = this.artists.filter(a => a.slug == instanceSlug)[0];
+          break;
+        case "genre":
+          entity = this.genres.filter(a => a.slug == instanceSlug)[0];
+          break;
+        case "playlist":
+          entity = this.playlists.filter(a => a.slug == instanceSlug)[0];
+          break;
+        case "source":
+          entity = this.source || {};
+          break;
+      }
+      if (entity) {
+        entity.rank = rank.rank;
+        entity.count = rank.total;
+      }
+    });
+
+  }
 }
