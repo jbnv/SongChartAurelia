d63c4c95233a23fadcfd3e464488afd7438afe7a
Jay Bienvenu <bienvenunet@yahoo.com>
2016-04-06 08:44:57 -0500

    Remove unneeded code.

--- src/song.js
+++ src/song.js
@@ -4,15 +4,4 @@ export class Song extends Data {
 
   fetchRouteFn = (parameters) => 'song/'+parameters.slug;
 
-  title = '(Song)';
-
-  constructor(http) {
-    super(http);
-
-  }
-
-  massage = (inbound) => {
-    this.title = inbound.title;
-  }
-
 }
