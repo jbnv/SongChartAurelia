47d459919e5363dd4a206f9130d46fe46dd2832f
Jay Bienvenu <bienvenunet@yahoo.com>
2016-04-11 08:31:50 -0500

    Add members and cross-reference.

--- src/artist.js
+++ src/artist.js
@@ -17,20 +17,20 @@ export class Artist extends Data {
   score = 0.00;
   songAdjustedAverage = 0.00;
 
-  songTableModel = {
-    'showOnly':[
-      'rank','title','role','source','score',
-      'debutDate','debutRank','peakRank','duration'
-    ]
-  }
-
   massage = (inbound) => {
     this.type = inbound.type || {slug: "", title: "NOT SET"};
     this.genres = inbound.genres || [];
+    this.members = inbound.members || [];
+    this.xref = inbound.xref || [];
+    this.songTableModel = {
+      'showOnly':[
+        'rank','title','role','source','score',
+        'debutDate','debutRank','peakRank','duration'
+      ]
+    };
     this.songTableModel.songs = inbound.songs || [];
     this.score = numeral(inbound.score || 0).format("0.00");
     this.songAdjustedAverage = numeral(inbound.songAdjustedAverage || 0).format("0.00");
-    console.log("artist.js:massage",this.score,this.songAdjustedAverage);
   }
 
 }
