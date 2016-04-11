f6e75d225863250b1f90777cd430e34779419a69
Jay Bienvenu <bienvenunet@yahoo.com>
2016-04-11 13:51:38 -0500

    Account for reformatted data.

--- src/songs.js
+++ src/songs.js
@@ -1,12 +1,14 @@
 import {Data} from './data';
 
 export class Songs extends Data {
-  fetchRoute = 'songs';
-  title = 'Songs';
-
-  songs = [];
+  fetchRoute = 'songs/count/100';
+  subtitle = "";
 
   massage = (inbound) => {
-    this.songs = inbound;
+    if (inbound.filteredCount) {
+      this.subtitle = `top ${inbound.filteredCount} of ${inbound.totalCount}`;
+    } else {
+      this.subtitle = `${inbound.totalCount}`;
+    }
   }
 }
