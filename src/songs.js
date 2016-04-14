ed100c4b54f41f240a9dedf935649d3f69f6c257
Jay Bienvenu <bienvenunet@yahoo.com>
2016-04-13 22:30:54 -0500

    Generalize songs/:filter. Add sample filter.

--- src/songs.js
+++ src/songs.js
@@ -1,14 +1,42 @@
 import {Data} from './data';
 
 export class Songs extends Data {
-  fetchRoute = 'songs/count/100';
+
+  sampleSize = 100;
+
+  fetchRouteFn = (parameters) => {
+    if (!parameters.filter)
+      return 'songs/count/100';
+    if (parameters.filter === "sample")
+      return `songs/sample/${this.sampleSize}`;
+    return 'songs/'+parameters.filter;
+  }
+
   subtitle = "";
 
   massage = (inbound) => {
+
+    if (this.parameters.filter) {
+      this.filter = this.parameters.filter.replace(":","-");
+    } else {
+      this.filter = null;
+    }
+    
+    if (this.parameters.filter === "sample") {
+      this.subtitle = this.sampleSize;
+      return;
+    }
+
+    if (this.parameters.filter === "unscored") {
+      this.subtitle = `${inbound.filteredCount} of ${inbound.totalCount}`;
+      return;
+    }
+
     if (inbound.filteredCount) {
       this.subtitle = `top ${inbound.filteredCount} of ${inbound.totalCount}`;
-    } else {
-      this.subtitle = `${inbound.totalCount}`;
+      return;
     }
+
+    this.subtitle = `${inbound.totalCount}`;
   }
 }
