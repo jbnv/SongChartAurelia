ec457ec6290d9040e4e80f83804edae55fba1bc7
Jay Bienvenu <bienvenunet@yahoo.com>
2016-04-14 21:57:17 -0500

    Factor refresh().

--- src/data.js
+++ src/data.js
@@ -23,21 +23,26 @@ export class Data {
     this.http = http;
   }
 
-  activate(parameters,routeConfig) {
-    this.parameters = parameters;
-    this.slug = parameters.slug;
+  refresh() {
     let fetchRoute = this.fetchRoute;
     if (this.fetchRouteFn) {
-      fetchRoute = this.fetchRouteFn(parameters);
+      fetchRoute = this.fetchRouteFn(this.parameters);
     }
     return this.http.fetch(fetchRoute)
       .then(response => response.json())
       .then(data => {
-        data.instanceSlug = data.instanceSlug || parameters.slug;
+        data.instanceSlug = data.instanceSlug || this.slug;
         Object.keys(data).forEach(key => this[key] = data[key]);
         this.massage(data);
-        routeConfig.navModel.setTitle(this.title);
+        this.navModel.setTitle(this.title);
       });
+  }
+
+  activate(parameters,routeConfig) {
+    this.parameters = parameters;
+    this.slug = parameters.slug;
+    this.navModel = routeConfig.navModel;
+    return this.refresh();
   }
 
 }
