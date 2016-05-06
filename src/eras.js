725512832d8370d36228d1c38ef678d806368734
Jay Bienvenu <bienvenunet@yahoo.com>
2016-05-06 15:06:41 -0500

    Apply new graph components to /eras.

--- src/eras.js
+++ src/eras.js
@@ -1,71 +1,8 @@
 import {Data} from './data';
-
-function colorStyle(countScale,aaScale) {
-  let r = aaScale/countScale;
-  let highlight = "";
-  if (r >= 1.2) {
-    highlight = "leader";
-  } else if (r <= 0.8) {
-    highlight = "lagger";
-  }
-  return highlight;
-}
+import numeral from 'numeral';
 
 export class Eras extends Data {
 
   fetchRouteFn = (parameters) => 'eras'; // returns array of songs
-
-  decadeCountScales = [];
-  decadeAaScales = [];
-  yearCountScales = [];
-  yearAaScales = [];
-
-  massage = (inbound) => {
-    this.decadeCountScales = [];
-    this.decadeAaScales = [];
-    this.yearCountScales = [];
-    this.yearAaScales = [];
-
-    Object.keys(inbound.decades).sort().forEach(key => {
-
-      let decade = inbound.decades[key];
-      decade.slug = key;
-      decade.title = key;
-
-      this.decadeCountScales.push({
-        title: decade.title,
-        scale: decade.songCountScale,
-        route: "decade/"+decade.slug
-      });
-      this.decadeAaScales.push({
-        title: decade.title,
-        scale: decade.songAdjustedAverageScale,
-        route: "decade/"+decade.slug,
-        highlight: colorStyle(decade.songCountScale,decade.songAdjustedAverageScale)
-      });
-
-    });
-
-    Object.keys(inbound.years).sort().forEach(key => {
-
-      let year = inbound.years[key];
-      year.slug = key;
-      year.title = key;
-
-      var title = ""+year.title.substr(2,1)+" "+year.title.substr(3,1);
-      this.yearCountScales.push({
-        title: title,
-        scale: year.songCountScale,
-        route: "year/"+year.slug
-      });
-      this.yearAaScales.push({
-        title: title,
-        scale: year.songAdjustedAverageScale,
-        route: "year/"+year.slug,
-        highlight: colorStyle(year.songCountScale,year.songAdjustedAverageScale)
-      });
-
-    });
-  }
 
 }
